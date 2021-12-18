901ee72554053dee1d4aeeb9b62157652c8f469a
Kai Wang <kevin.wang.part.time@gmail.com>
2021-12-18 20:59:46 +0900

    Updated to step 35

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,4 +1,5 @@
-is_simple_core = True
+# is_simple_core = True
+is_simple_core = False
 
 if is_simple_core:
 	from dezero.core_simple import Variable
@@ -10,6 +11,12 @@ if is_simple_core:
 	from dezero.core_simple import setup_variable
 
 else:
-	pass
+	from dezero.core import Variable
+	from dezero.core import Function
+	from dezero.core import using_config
+	from dezero.core import no_grad
+	from dezero.core import as_array
+	from dezero.core import as_variable
+	from dezero.core import setup_variable
 
 setup_variable()
---
+++ steps/step28.py
@@ -0,0 +1,33 @@
+if '__file__' in globals():
+	import os, sys
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+
+
+# Minimum at (1, 1)
+def rosenbrock(x0, x1):
+	y = 100 * (x1 - x0 ** 2) ** 2 + (x0 - 1) ** 2
+	return y
+
+
+x0 = Variable(np.array(0.0))
+x1 = Variable(np.array(2.0))
+lr = 0.001
+iters = 10000
+
+# Approximate optimization by gadient descent
+for i in range(iters):
+
+	y = rosenbrock(x0, x1)
+
+	x0.cleargrad()
+	x1.cleargrad()
+	y.backward()
+
+	x0.data -= lr * x0.grad
+	x1.data -= lr * x1.grad
+
+# variable(0.9944984367782456) variable(0.9890050527419593)
+print(x0, x1)
