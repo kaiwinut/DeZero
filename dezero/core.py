ef0f6fd279b50af9e23ff9f9850890208906119d
Kai Wang <kevin.wang.part.time@gmail.com>
2021-12-18 22:35:40 +0900

    Update and bug fixes until step 40

--- dezero/core.py
+++ dezero/core.py
@@ -1,6 +1,7 @@
 import weakref
 import contextlib
 import numpy as np
+import dezero
 
 
 class Config:
@@ -76,6 +77,17 @@ class Variable:
 	def cleargrad(self):
 		self.grad = None	
 
+	def reshape(self, *shape):
+		if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+			shape = shape[0]
+		return dezero.functions.reshape(self, shape)
+
+	def tranpose(self):
+		return dezero.functions.tranpose(self)
+
+	def sum(self, axis = None, keepdims = False):
+		return dezero.functions.sum(self, axis, keepdims)
+
 	# Enables user to access the shape property with x.shape instead of x.shape()
 	@property	
 	def shape(self):
@@ -92,6 +104,10 @@ class Variable:
 	@property	
 	def dtype(self):
 		return self.data.dtype
+
+	@property
+	def T(self):
+		return dezero.functions.tranpose(self)
 
 	def __len__(self):
 		return len(self.data)
@@ -136,11 +152,16 @@ class Function:
 
 class Add(Function):
 	def forward(self, x0, x1):
+		self.x0_shape, self.x1_shape = x0.shape, x1.shape
 		y = x0 + x1
 		return y
 
 	def backward(self, gy):
-		return gy, gy
+		gx0, gx1 = gy, gy
+		if self.x0_shape != self.x1_shape:
+			gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+			gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+		return gx0, gx1
 
 
 class Mul(Function):
--- dezero/functions.py
+++ dezero/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
-from dezero.core import Function
+from dezero import utils
+from dezero.core import Function, Variable, as_variable, as_array
 
 
 class Sin(Function):
@@ -35,6 +36,75 @@ class Tanh(Function):
 		return gx
 
 
+class Reshape(Function):
+	def __init__(self, shape):
+		self.shape = shape
+
+	def forward(self, x):
+		self.x_shape = x.shape
+		y = x.reshape(self.shape)
+		return y
+
+	def backward(self, gy):
+		return reshape(gy, self.x_shape)
+
+
+class Transpose(Function):
+	def forward(self, x):
+		y = np.transpose(x)
+		return y
+
+	def backward(self, gy):
+		gx = transpose(gy)
+		return gx
+
+
+class Sum(Function):
+	def __init__(self, axis, keepdims):
+		self.axis = axis
+		self.keepdims = keepdims
+
+	def forward(self, x):
+		self.x_shape = x.shape
+		y = x.sum(axis = self.axis, keepdims = self.keepdims)
+		return y
+
+	def backward(self, gy):
+		gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
+		gx = broadcast_to(gy, self.x_shape)
+		return gx
+
+
+class BroadcastTo(Function):
+	def __init__(self, shape):
+		self.shape = shape
+
+	def forward(self, x):
+		self.x_shape = x.shape
+		y = np.broadcast_to(x, self.shape)
+		return y
+
+	def backward(self, gy):
+		# For example, y = x + x, when x is used twice, 
+		# the gradient is the sum of the two variables used
+		gx = sum_to(gy, self.x_shape)
+		return gx
+
+
+class SumTo(Function):
+	def __init__(self, shape):
+		self.shape = shape
+
+	def forward(self, x):
+		self.x_shape = x.shape
+		y = utils.sum_to(x, self.shape)
+		return y
+
+	def backward(self, gy):
+		gx = broadcast_to(gy, self.x_shape)
+		return gx
+
+
 def sin(x):
 	return Sin()(x)
 
@@ -45,3 +115,29 @@ def cos(x):
 
 def tanh(x):
 	return Tanh()(x)
+
+
+def reshape(x, shape):
+	if x.shape == shape:
+		return as_variable(x)
+	return Reshape(shape)(x)
+
+
+def transpose(x):
+	return Transpose()(x)
+
+
+def sum(x, axis = None, keepdims = False):
+	return Sum(axis, keepdims)(x)
+
+
+def broadcast_to(x, shape):
+	if x.shape == shape:
+		return as_variable(x)
+	return BroadcastTo(shape)(x)
+
+
+def sum_to(x, shape):
+	if x.shape == shape:
+		return as_variable(x)
+	return SumTo(shape)(x)
